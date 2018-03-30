f60d33476fce4011caab2edb57bf9a53e62f5ec0
Daniel Wedin <danielwedin@hotmail.se>
2018-03-30 13:16:18 +0200

    Update path to ant css

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link rel="stylesheet" href="https://unpkg.com/antd@3/dist/antd.min.css" />
+          <link rel="stylesheet" href="https://unpkg.com/antd@3.3.3/dist/antd.min.css" />
           <meta name="Description" content="Sparkalkylator med ränta på ränta effekten." />
         </Head>
         <body>
